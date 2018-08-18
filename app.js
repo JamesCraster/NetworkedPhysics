1b40801d20dc908c0e516efd930abdb6b75c34af
JamesCraster <jamesvaughancraster@gmail.com>
2018-08-18 19:22:24 +0100

    updated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*var express = require("express");
+var express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
@@ -38,6 +38,11 @@ class Player extends Rectangle {
         this.vx = 0;
         this.vy = 0;
         this.grounded = false;
+        this.directions = [Inputs.NONE, Inputs.NONE];
+    }
+
+    update() {
+
     }
 }
 
@@ -97,7 +102,7 @@ io.on("connection", function (socket) {
             player.position.x = 750;
             player.vx = 0;
         }
-        /*for (let i = 0; i < boxes.length; i++) {
+        for (let i = 0; i < boxes.length; i++) {
             //top edge
             if (player.vy > 0) {
                 if (hitTestRectangle(player.project(0, player.vy), boxes[i])) {
@@ -133,102 +138,12 @@ io.on("connection", function (socket) {
             }
         }
 
-player.position.x += player.vx;
-player.position.y += player.vy;
-socket.emit('update', player.position);
-}
-setInterval(update, 100);
-});
-
-var port = 8081;
-http.listen(port, function () {
-    console.log('Port is:' + port);
-});*/
-
-var express = require("express");
-var app = express();
-var http = require('http').Server(app);
-var io = require("socket.io")(http);
-
-app.use(express.static('public'));
-
-var Inputs = {
-    LEFT: 'LEFT',
-    RIGHT: 'RIGHT',
-    DOWN: 'DOWN',
-    UP: 'UP',
-    NONE: 'NONE'
-}
-
-io.on("connection", function (socket) {
-    let directions = [Inputs.NONE, Inputs.NONE];
-    let player = {
-        vx: 0,
-        vy: 0,
-        grounded: false
-    };
-    let position = {
-        x: 0,
-        y: 0
-    };
-
-    socket.on('input', function (direction) {
-        if (direction == Inputs.LEFT || direction == Inputs.RIGHT) {
-            directions[0] = direction;
-        }
-        if (direction == Inputs.UP || direction == Inputs.DOWN) {
-            directions[1] = direction;
-        }
-    });
-
-    function update() {
-        if (directions[0] == Inputs.LEFT) {
-            player.vx -= 4;
-        } else if (directions[0] == Inputs.RIGHT) {
-            player.vx += 4;
-        }
-        if (directions[1] == Inputs.UP) {
-            if (player.grounded == true) {
-                player.vy -= 50;
-                player.grounded = false;
-            }
-            directions[1] = Inputs.NONE;
-        }
-        player.vy += 5;
-
-        if (player.vx > 20) {
-            player.vx = 20;
-        }
-        if (player.vx < -20) {
-            player.vx = -20;
-        }
-        if (player.vy > 30) {
-            player.vy = 30;
-        }
-
-        if (position.y + player.vy > 550) {
-            position.y = 550;
-            player.vy = 0;
-            player.grounded = true;
-        }
-        if (position.x + player.vx < 0) {
-            position.x = 0;
-            player.vx = 0;
-        }
-        if (position.x + player.vx > 750) {
-            position.x = 750;
-            player.vx = 0;
-        }
-        position.x += player.vx;
-        position.y += player.vy;
-        socket.emit('update', position);
+        player.position.x += player.vx;
+        player.position.y += player.vy;
+        socket.emit('update', player.position);
     }
     setInterval(update, 100);
 });
-
-function update() {
-
-}
 
 var port = 8081;
 http.listen(port, function () {
